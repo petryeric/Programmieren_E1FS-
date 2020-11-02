5d0e6dbcd0baf2903f5bb0283a3a8a0c09f54869
Eric Petry <eric.petry@mail.schwarz>
2020-11-02 09:59:51 +0100

    Aufgabe if statement

---
+++ Program_ifstatement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IfStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 17;
+            bool premium = true;
+
+            if(alter >= 18 && premium == true)
+            {
+                Console.WriteLine("Sie dürfen bei uns Filme Streamen, Willkommen und viel Spaß!");
+            }
+            else
+            {
+                Console.WriteLine("Sie dürfen keine Filme bei uns Streamen, Fuck You !");
+            }
+        }
+    }
+}
