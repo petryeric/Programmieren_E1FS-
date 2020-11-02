e614c2aa40de3e371e94618d089c171cdbe7928d
Eric Petry <eric.petry@mail.schwarz>
2020-11-02 08:45:46 +0100

    Erste Aufgaben SAEL

---
+++ Schule_c#.cs
@@ -0,0 +1,37 @@
+using System.Reflection.Emit;
+using System.Collections.Concurrent;
+using System.Data;
+using Internal;
+using System;
+namespace türsteher
+{
+    class program
+    {
+        static Void main(string[]args)
+    }{
+        int zahl = 5;
+        int zahl2 = 6;
+
+        int Ergebnis = zahl + zahl2;
+
+        int zahl3 = 6;
+        int zahl4 = 3;
+        Ergebnis2 = zahl3/zahl4;
+        Console.WriteLine(Ergebnis2);
+        Console.ReadKey();
+
+        int zahl5 = 25;
+        int zahl6 =5;
+        Ergebnis3 = zahl5%zahl6;
+
+        // beim Modulo Operator wird der Rest einer Division berechnet und ausgegeben
+
+
+
+
+
+
+
+      
+    }
+}
