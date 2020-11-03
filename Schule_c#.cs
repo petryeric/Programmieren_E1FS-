f0452dff0c0f0eb597e870002469966f893bb3d9
Eric Petry <eric.petry@mail.schwarz>
2020-11-03 14:48:48 +0100

    hinzufügen einer methode und definition eines objektes

--- Schule_c#.cs
+++ Schule_c#.cs
@@ -7,25 +7,30 @@ namespace türsteher
 {
     class program
     {
-        static Void main(string[]args)
-    }{
-        int zahl = 5;
-        int zahl2 = 6;
+        static Void main(string[] args)
+        {
 
-        int Ergebnis = zahl + zahl2;
+            int zahl = 5;
+            int zahl2 = 6;
 
-        int zahl3 = 6;
-        int zahl4 = 3;
-        Ergebnis2 = zahl3/zahl4;
-        Console.WriteLine(Ergebnis2);
-        Console.ReadKey();
+            int Ergebnis = zahl + zahl2;
 
-        int zahl5 = 25;
-        int zahl6 =5;
-        Ergebnis3 = zahl5%zahl6;
+            int zahl3 = 6;
+            int zahl4 = 3;
+            Ergebnis2 = zahl3 / zahl4;
+            Console.WriteLine(Ergebnis2);
+            Console.ReadKey();
+
+            int zahl5 = 25;
+            int zahl6 = 5;
+            Ergebnis3 = zahl5 % zahl6;
+            Console.WriteLine(Ergebnis3);
+            Console.ReadKey;
 
         // beim Modulo Operator wird der Rest einer Division berechnet und ausgegeben
 
+        }
+    }
 
 
 
