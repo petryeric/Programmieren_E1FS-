76d85d2764db186163e017e52444801077e2b568
Eric Petry <eric.petry@mail.schwarz>
2020-11-16 10:03:51 +0100

    Schleifen

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zähler = 0;
+            Console.WriteLine("Welche Art von Zahl soll angegeben werden?");
+            string input = Console.ReadLine();
+           
+            while (Zähler <= 100)
+            {
+                if (Zähler % 2 == 0 && input == "gerade")
+                {
+                    Console.WriteLine(Zähler + " ist eine gerade Zahl");
+
+                }
+                else if (Zähler % 2 == 1 && input == "ungerade") 
+                {
+                    Console.WriteLine(Zähler + " ist eine ungerade Zahl");
+                }
+             
+                Zähler++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
