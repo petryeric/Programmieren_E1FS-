b7c9e35019edc0e1312d124eb192e4df9af2dbbc
Eric Petry <eric.petry@mail.schwarz>
2021-04-19 09:54:52 +0200

    Fakultät mit Array

---
+++ Fakultät_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fakultät
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Fakultät von n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int resultcounter = 0;
+
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+                resultcounter++;
+                int[] results = new int[resultcounter];
+                result = results[resultcounter];
+
+            }
+
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
