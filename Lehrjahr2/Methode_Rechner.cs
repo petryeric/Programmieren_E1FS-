1cc16b0b1f6ac67aec782d49e77eb3bcd8be9340
Eric Petry <eric.petry@mail.schwarz>
2021-09-13 10:49:47 +0200

    Methode mit Rechenoperation

---
+++ Lehrjahr2/Methode_Rechner.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Übungsaufgabe_13092021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+           
+
+            Console.WriteLine(program.Rechner(5.0, 6.0));
+
+        }
+
+        public static double Rechner(double zahl1, double zahl2)
+        {
+            Console.WriteLine("Geben Sie die Rechenoperation ein");
+            string Operation = Console.ReadLine();
+
+            switch (Operation)
+            {
+                case "Addition":
+                    return zahl1 + zahl2;
+                    
+                case "Subtraktion":
+                    if (zahl1 < zahl2)
+                    {
+                        return -(zahl2 - zahl1);
+                    }
+                    else
+                    {
+                        return zahl1 - zahl2;
+                    }
+                   
+                case "Multiplikation":
+                    return zahl1 * zahl2;
+                    
+                case "Division":
+                    double Ergebnis = zahl1 / zahl2;
+                    return Ergebnis;
+                case "Mod":
+                    return zahl1 % zahl2;
+                   
+                default:
+                    Console.WriteLine("Bitte geben Sie eine korrekte Operation ein");
+                    
+                    return 0;
+
+            }
+
+
+
+
+
+        }        
+            }
+
+
+
+        }
+
+   
+
