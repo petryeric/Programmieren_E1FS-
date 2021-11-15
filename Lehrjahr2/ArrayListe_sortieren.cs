bc35bfbb294c8079dfdcc5f13fc3576b595760d3
Eric Petry <eric.petry@mail.schwarz>
2021-11-15 08:31:23 +0100

    adding solution for ex.2

---
+++ Lehrjahr2/ArrayListe_sortieren.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace Aufgabe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList Warteliste = new ArrayList();
+
+            Warteliste.Add("Luca");
+            Warteliste.Add("Dennis");
+            Warteliste.Add("Boris");
+            Warteliste.Add("Leon");
+            Warteliste.Add("Hakki");
+            Warteliste.Add("Nico");
+            Warteliste.Add("Franziska");
+
+            Warteliste.Sort();
+
+            foreach(object o in Warteliste)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
